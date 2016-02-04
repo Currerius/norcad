f2d5d729e3dd952f4fd05e5c30a7cf03427c9489
Currerius <reinhard@currerius.com>
2016-02-04 23:33:52 +0100

    Anonymous script ongoing work. Started work on metadata.

--- ParseJSON.py
+++ ParseJSON.py
@@ -3,32 +3,90 @@
  
 import json
 import pprint
+import requests
 import couchdb # python2 only
-
-## python3
-# with open('survival.json', encoding='utf-8') as data_file:
-#     survJSON = json.loads(data_file.read())
-## or
-# survJSON = json.loads(open('survival.json',encoding='utf-8').read())
 
 survJSON = json.loads(open('survival.json').read())
 
+
 Events = []
-for var in survJSON["Endpoints"]:  Events.append( var["Event"] )
+for var in survJSON["Endpoints"]:
+  Events.append( var["Event"] )
+
+EventDBaliases = []
+for i in range(len(Events)):
+  EventDBaliases.append( Events[i]+" AS E"+`i`)
+
+
 
 Times = []
-for var in survJSON["Endpoints"]:  Times.append( var["Time"] )
+for var in survJSON["Endpoints"]:
+  Times.append( var["Time"] )
+
+TimeDBaliases = []
+for i in range(len(Times)):
+  TimeDBaliases.append( Times[i]+" AS T"+`i`)
+
 
 Predictors = []
-for var in survJSON["Predictors"]:  Predictors.append( var["Name"] )
+for var in survJSON["Predictors"]:
+  Predictors.append( var["Name"] )
+
+PredictorDBaliases = []
+for i in range(len(Predictors)):
+  PredictorDBaliases.append( Predictors[i]+" AS P"+`i`)
+
+
 
 Modifiers = []
-for var in survJSON["Modifiers"]:  Modifiers.append( var["Name"] )
+for var in survJSON["Modifiers"]:
+  Modifiers.append( var["Name"] )
+
+ModifierDBaliases = []
+for i in range(len(Modifiers)):
+  ModifierDBaliases.append( Modifiers[i]+" AS M"+`i`)
+
+
 
 Adjustments = []
-for var in survJSON["Adjustments"]:  Adjustments.append( var["Name"] )
+for var in survJSON["Adjustments"]:
+  Adjustments.append( var["Name"] )
 
-sqllist = Events + Times + Predictors + Modifiers + Adjustments
+AdjustmentDBaliases = []
+for i in range(len(Adjustments)):
+  AdjustmentDBaliases.append( Adjustments[i]+" AS A"+`i`)
+
+
+
+SQLselect = "SELECT "
+
+for var in EventDBaliases:
+  SQLselect += (var + ", ")
+
+for var in TimeDBaliases:
+  SQLselect += (var + ", ")
+
+for var in PredictorDBaliases:
+  SQLselect += (var + ", ")
+
+for var in AdjustmentDBaliases:
+  SQLselect += (var + ", ")
+
+for var in ModifierDBaliases:
+  SQLselect += (var + ", ")
+
+SQLselect = SQLselect[:-2]
+
+
+
+SQLwhere = "WHERE "
+for var in survJSON["Population"]["Subset"]:
+  SQLwhere += var["Where"]+" AND "
+
+SQLwhere = SQLwhere[:-4]
+
+
+DBstatement = SQLselect + " FROM PATIENT_INFO P LEFT JOIN LAB_BLOOD_TOTAL B ON P.FORUS_ID=B.FORUS_ID LEFT JOIN hs_medikament_skjema M ON P.FORUS_ID=M.FORUS_ID LEFT JOIN hs_hendelser_CVDNOR_mars14 C ON P.NORCAD_ID=C.NORCAD_ID" + SQLwhere + ";"
 
 
 # since localhost is default shorten it
